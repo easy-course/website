6cb2b5de75f1cfcff369cb65b16e3da9baf6631d
pduret <pierrelouis.duret@gmail.com>
2016-03-20 15:44:12 +0100

    blabla

--- website/app.js
+++ website/app.js
@@ -1,13 +1,15 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
     form = require('express-form'),
-    field = form.field;
-var app = express();
- 
-var hbs = require('hbs');
- 
-var blogEngine = require('./call_personne');
- 
+    field = form.field,
+    fs = require("fs"),
+    hbs = require('hbs');
+    blogEngine = require('./call_personne');
+
+
+var app = express('./call_personne');
+var json = fs.readFileSync("personnes.json", "UTF-8");
+var parsed = JSON.parse(json);
  
 app.use(express.static('public'));
 app.set('view engine', 'html');
@@ -31,22 +33,48 @@ app.get('/inscription',
 			   console.log(req.form.errors);
 		 
 			} 
-			else if(req.query['username'] != 'undefined') 
+			else if(req.query['pseudo'] != null) 
 			{
-			   console.log('Pseudo: ' + req.query['pseudo']);
-			   console.log('Email: ' + req.query['email']);
-			   console.log('Ville: ' + req.query['city']);
-			   console.log('Code Postal: ' + req.query['postal']);
-			   console.log('Competence: ' + req.query['skill']);
-			   console.log('Description: ' + req.query['description']);
+				console.log('Pseudo: ' + req.query['pseudo']);
+				console.log('Email: ' + req.query['email']);
+				console.log('Ville: ' + req.query['city']);
+				console.log('Code Postal: ' + req.query['postal']);
+				console.log('Competence: ' + req.query['skill']);
+				console.log('Description: ' + req.query['description']);
+
+			    var entries = [];
+				for(var x in parsed)
+					entries.push(parsed[x]);
+
+
+				entries.push({
+					id: entries.length + 1,
+					pseudo: req.query['pseudo'],
+					email: req.query['email'],
+					ville: req.query['city'],
+					postal: req.query['postal'],
+					//longitude: latitude.toString(),
+					//latitude: longitude.toString(),
+					competence: req.query['skill'],
+					description: req.query['description']
+				});
+
+
+				fs.writeFile('personnes.json', JSON.stringify(entries, null, 4), function(err)
+				{
+					if(err) {console.log(err);}
+				});
+
+
+
 			}
 });
 
 
 app.get('/inscription.html/:p1', function(request, response) {
-  var p1 = request.params.p1; 
-  console.log(p1);
-  response.sendFile( __dirname  + '/inscription.html');
+	var p1 = request.params.p1; 
+	console.log(p1);
+	response.sendFile( __dirname  + '/inscription.html');
 });
 
 
--- website/call_personne.js
+++ website/call_personne.js
@@ -1,26 +1,10 @@
-//fs.writeFileSync("personnes", "contenu du fichier", "UTF-8");
-
 var fs = require("fs");
 var json = fs.readFileSync("personnes.json", "UTF-8");
-//json = require('personnes.json');
-
 var parsed = JSON.parse(json);
 
 var entries = [];
 for(var x in parsed)
 	entries.push(parsed[x]);
-
-entries.push(
-    {pseudo: "XX", email: "XX", ville: "XX", longitude: "XX", latitude: "XX", competence: [{ domaine: "XX", specialite: "XX" }]        }
-);
-
-
-fs.writeFile('personnes.json', JSON.stringify(entries, null, 4), function(err)
-			{
-				if(err) {console.log(err);}
-				
-			});
-
  
 exports.getBlogEntries = function() {
     return entries;
