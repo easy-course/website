37c468105e36ed3278d01dc534c039b73a320087
voillot.virgil@gmail.com <voillot.virgil@gmail.com>
2016-03-21 23:16:15 +0100

    bon app

--- website/app.js
+++ website/app.js
@@ -4,7 +4,9 @@ var express = require('express'),
     field = form.field,
     fs = require("fs"),
     hbs = require('hbs');
-    blogEngine = require('./call_personne');
+var blogEngine = require('./call_personne');
+	
+var ejs = require('ejs');
 
 
 var app = express('./call_personne');
@@ -16,16 +18,16 @@ app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.bodyParser());
  
+ 
 app.get('/', function(req, res) {
-    res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
+    res.render('index',{entries:blogEngine.getBlogEntries()});
 });
+
 
 app.get('/inscription', 
 		form(
-			//field("username").trim().required().is(/^[a-z]+$/),
-			//field("password").trim().required().is(/^[0-9]+$/),
-			//field("email").trim().isEmail()
-		   ),
+			field("search").trim()
+		),
 		function(req, res) {
 			res.render('inscription', {title:"About Me"});
 			if (!req.form.isValid) {
@@ -33,46 +35,44 @@ app.get('/inscription',
 			   console.log(req.form.errors);
 		 
 			} 
-			else if(req.query['pseudo'] != null && req.query['pseudo']!='' && req.query['email'] != null) 
-			{
-				console.log('Pseudo: ' + req.query['pseudo']);
-				console.log('Email: ' + req.query['email']);
-				console.log('Ville: ' + req.query['city']);
-				console.log('Code Postal: ' + req.query['postal']);
-				console.log('Competence: ' + req.query['skill']);
-				console.log('Description: ' + req.query['description']);
-
-			    var entries = [];
-				for(var x in parsed)
-					entries.push(parsed[x]);
-
-
-				entries.push({
-					id: entries.length + 1,
-					pseudo: req.query['pseudo'],
-					email: req.query['email'],
-					ville: req.query['city'],
-					postal: req.query['postal'],
-					//longitude: latitude.toString(),
-					//latitude: longitude.toString(),
-					competence: req.query['skill'],
-					description: req.query['description']
-				});
-
-
-				fs.writeFile('personnes.json', JSON.stringify(entries, null, 4), function(err)
-				{
-					if(err) {console.log(err);}
-				});
-
-
-
-			}else if(req.query['search'] != null && req.query['search'] != '')
+			else if(req.query['search'] != null && req.query['search'] != '')
 			{
 				console.log('Recherche: ' + req.query['search']);
 			}
 });
-
+app.post('/inscription',
+  form(
+    field("pseudo").trim().required().is(/^[a-z]+$/),
+    field("city").trim().required().isAlphanumeric(),
+	field("postal").trim().required().is(/^[0-9]+$/),
+    field("email").trim().isEmail(),
+	field("skill").trim(),
+	field("description").trim()
+   ),
+   function(req, res){
+     if (!req.form.isValid) {
+       // Handle errors 
+       console.log(req.form.errors); 
+     } else {
+				var entries = [];
+				for(var x in parsed)
+					entries.push(parsed[x]);
+				entries.push({
+					id: entries.length + 1,
+					pseudo: req.form.pseudo,
+					email: req.form.email,
+					ville: req.form.city,
+					postal: req.form.postal,
+					competence: req.form.skill,
+					description: req.form.description
+				});
+				fs.writeFile('personnes.json', JSON.stringify(entries, null, 4), function(err)
+				{
+					if(err) {console.log(err);}
+				});
+     }
+  }
+);
 
 app.get('/inscription.html/:p1', function(request, response) {
 	var p1 = request.params.p1; 
